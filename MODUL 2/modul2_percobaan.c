744efd0f80c21c622f6c77742acf215a52fbc5b8
Isra23343045 <145670658+Isra23343045@users.noreply.github.com>
2023-12-08 16:22:45 +0700

    Add files via upload

---
+++ MODUL 2/modul2_percobaan.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int a = 1,b = 2,c;
+    char present[20];
+    printf("hello world\n");
+    printf("hai,ini adalah percobaan\n");
+    c = a + b;
+    printf("%i",c);
+    printf("apa hadiah yang ingin kamu kirimkan ? \n");
+    scanf("%s",&present);
+    printf("%s",present);
+
+    return 0;
+}
